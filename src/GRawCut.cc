329f17b4015bcf8e8432c7364245b4c768d0df63
Patrik Ott <ott@kph.uni-mainz.de>
2014-03-27 17:21:48 +0100

    bugfix GRawCut.cc

--- src/GRawCut.cc
+++ src/GRawCut.cc
@@ -18,32 +18,29 @@ GRawCut::~GRawCut()
 
 void  GRawCut::ProcessEvent()
 {
-    if(photons->GetNParticles() != nPhoton && nPhoton != -1)
+    if(rawEvent->GetNCB() != nPhoton && nPhoton != -1)
         return;
 
-    if(protons->GetNParticles() != nProton && nProton != -1)
+    if(rawEvent->GetNTAPS() != nProton && nProton != -1)
         return;
             
     eventFlags->Fill();
+    trigger->Fill();
     tagger->Fill();
-    photons->Fill();
-    protons->Fill();
 }
 
 Bool_t  GRawCut::Process(const char* input_filename, const char* output_filename)
 {
     if(!Open(input_filename))    return kFALSE;
-    if(!OpenPhotons())    return kFALSE;
-    if(!OpenProtons())    return kFALSE;
     if(!OpenTagger())    return kFALSE;
     if(!OpenScalers())    return kFALSE;
+    if(!OpenTrigger())    return kFALSE;
     if(!OpenEventFlags())    return kFALSE;
 
 
     if(!Create(output_filename))    return kFALSE;
-    if(!CreatePhotons())    return kFALSE;
-    if(!CreateProtons())    return kFALSE;
     if(!CreateTagger())    return kFALSE;
+    if(!CreateTrigger())    return kFALSE;
     if(!CreateEventFlags())    return kFALSE;
     scalers->Clone(*file_out);
 
