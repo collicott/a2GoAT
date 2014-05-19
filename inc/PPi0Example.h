659baa4d629d24328d22049302d6c7a8fe48a285
Cristina Collicott <collicott@dal.ca>
2014-05-19 18:21:13 +0200

    Simplified example Pi0 class

--- inc/PPi0Example.h
+++ inc/PPi0Example.h
@@ -9,70 +9,25 @@ using namespace std;
 
 #include "PPhysics.h"
 
-#define maxentries	1024
-
 class	PPi0Example : public PPhysics
 {
 private:
 
-	Char_t* gfile;
-	Char_t* tfile;
-	Char_t* hfile;
-
-	TFile* 	PhysTreeFile;
-	TTree*	treePhysics;
-	
-	Bool_t	ReconstructPhysicsTree;
-	Bool_t	FillHistograms;
-	
-	Double_t MMCut_low;
-	Double_t MMCut_high;
-	
-	Double_t theta_bin_size;
-	Double_t phi_bin_size;
-	
+	Double_t time;
 	TH1* 	time_pi0;
 	TH1* 	time_pi0_cuts;	
 
-	TH1* 	TC_prompt;
-	TH1* 	TC_random;
-	TH1* 	TC_sub;
-
-	TH1* 	E_prompt;
-	TH1* 	E_random;
-	TH1* 	E_sub;
-
-	TH1* 	theta_prompt;
-	TH1* 	theta_random;
-	TH1* 	theta_sub;
-
-	TH1* 	phi_prompt;
-	TH1* 	phi_random;
-	TH1* 	phi_sub;
-	
 	TH1* 	MM_prompt_pi0;
 	TH1* 	MM_random_pi0;
-	TH1* 	MM_sub_pi0;
+	TH1* 	MM_pi0;
 	
-	TH1* 	ME_prompt_pi0;
-	TH1* 	ME_random_pi0;
-	TH1* 	ME_sub_pi0;
-	
-	TH3*	TC_theta_phi_prompt;
-	TH3*	TC_theta_phi_random;
-	
-	Int_t 	nentries;
-	Int_t*	TC;
-	Double_t* E;
-	Double_t* time;
-	Double_t* theta;
-	Double_t* phi;
-	Double_t* MissMass;
-	Double_t* MissE;	
-	
-	Bool_t 	Prompt;
-	Bool_t 	Random;
-	
+	TH1* 	MM_prompt_pi0_n_2g;
+	TH1* 	MM_random_pi0_n_2g;
+	TH1* 	MM_pi0_n_2g;		
+
+	Int_t 	N_pi0;
+
+		
 protected:
 
 
@@ -80,24 +35,14 @@ public:
     PPi0Example();
     virtual ~PPi0Example();
 
-    virtual Bool_t	Init(const char* configfile);
-    virtual Bool_t	File(const char* gfile, const char* tfile, const char* hfile);
+    virtual Bool_t	Init(const char* configfile);	
+    virtual Bool_t	File(const char* file_in, const char* file_out);    
     virtual void 	Analyse();
 	virtual void	Reconstruct();
 	void	PostReconstruction();
 
-	void	InitHistograms();
-	Bool_t	WriteHistograms(TFile* hfile);
+	void	DefineHistograms();
+	Bool_t	WriteHistograms(TFile* pfile);
 	Bool_t	WriteHistograms() {return WriteHistograms(HistFile);}
-	
-	Bool_t 	OpenPhysTreeFile(const char* pfile, Option_t* option);
-	Bool_t 	OpenPhysTreeFile(const char* pfile) { return OpenPhysTreeFile(pfile,"RECREATE");}
-	Bool_t	ClosePhysTreeFile();		
-	Bool_t 	InitPhysicsTree(TFile* F);
-	Bool_t	OpenPhysicsTree(TFile* F);
-	Bool_t	OpenPhysicsTree() 	{return OpenPhysicsTree(PhysTreeFile);}	
-	Bool_t 	WriteTrees(TFile* F);
-	Bool_t 	WriteTrees()		{return WriteTrees(PhysTreeFile);}
-
 };
 #endif
--- src/PPi0Example.cc
+++ src/PPi0Example.cc
@@ -2,8 +2,15 @@
 
 #include "PPi0Example.h"
 
+/**
+ * @brief the main routine
+ * @param argc number of parameters
+ * @param argv the parameters as strings
+ * @return exit code
+ */
 int main(int argc, char *argv[])
 {
+
 	clock_t start, end;
 	start = clock();
 
@@ -13,11 +20,9 @@ int main(int argc, char *argv[])
 	std::string dir_in = "";
 	std::string dir_out = "";
 	std::string file_in = "";
-	std::string tree_out = "";
-	std::string hist_out = "";	
+	std::string file_out = "";
 	std::string pre_in = "";
-	std::string pre_tree = "Pi0Tree";
-	std::string pre_hist = "Pi0Hist";
+	std::string pre_out = "";
 
 	Int_t length;
 	std::string flag;
@@ -41,7 +46,9 @@ int main(int argc, char *argv[])
 				else if(strcmp(flag.c_str(), "d") == 0) dir_in = argv[i];
 				else if(strcmp(flag.c_str(), "D") == 0) dir_out = argv[i];
 				else if(strcmp(flag.c_str(), "f") == 0) file_in = argv[i];
+				else if(strcmp(flag.c_str(), "F") == 0) file_out = argv[i];
 				else if(strcmp(flag.c_str(), "p") == 0) pre_in = argv[i];
+				else if(strcmp(flag.c_str(), "P") == 0) pre_out = argv[i];
 				else
 				{
 					cout << "Unknown flag " << flag << endl;
@@ -99,6 +106,13 @@ int main(int argc, char *argv[])
 		if(strcmp(flag.c_str(),"nokey") != 0) file_in = flag;
 	}
 	
+	if(file_out.length() == 0)
+	{	
+		flag = ppi0->ReadConfig("Output-File",0,(Char_t*)serverfile.c_str());	
+		flag.erase(0,flag.find_first_not_of(" "));
+		if(strcmp(flag.c_str(),"nokey") != 0) file_out = flag;
+	}
+	
 	if(pre_in.length() == 0)
 	{	
 		flag = ppi0->ReadConfig("Input-Prefix",0,(Char_t*)serverfile.c_str());	
@@ -106,6 +120,12 @@ int main(int argc, char *argv[])
 		if(strcmp(flag.c_str(),"nokey") != 0) pre_in = flag;
 	}
 	
+	if(pre_out.length() == 0)
+	{	
+		flag = ppi0->ReadConfig("Output-Prefix",0,(Char_t*)serverfile.c_str());	
+		flag.erase(0,flag.find_first_not_of(" "));
+		if(strcmp(flag.c_str(),"nokey") != 0) pre_out = flag;
+	}
 	// Finished scanning for file settings
 	
 	// Fix directories to include final slash if not there
@@ -118,8 +138,11 @@ int main(int argc, char *argv[])
 	if(dir_in.length() != 0)  	cout << "Input directory:  '" << dir_in << "' chosen" << endl;
 	if(dir_out.length() != 0)  	cout << "Output directory: '" << dir_out << "' chosen" << endl;
 	if(file_in.length() != 0)  	cout << "Input file:       '" << file_in << "' chosen" << endl;
+	if(file_out.length() != 0) 	cout << "Output file:      '" << file_out << "' chosen" << endl;
 	if(pre_in.length() != 0)  	cout << "Input prefix:     '" << pre_in << "' chosen" << endl;
 	else { pre_in = "GoAT"; 	cout << "Input prefix:     '" << pre_in << "' chosen by default" << endl; }
+	if(pre_out.length() != 0)  	cout << "Output prefix:    '" << pre_out << "' chosen" << endl;	
+	else { pre_out = "Pi0"; 	cout << "Output prefix:    '" << pre_out << "' chosen by default" << endl; }
 	cout << endl;
 	
 	// Perform full initialisation 
@@ -148,26 +171,7 @@ int main(int argc, char *argv[])
 			cout << "Input file  '" << file_in << "' chosen" << endl;
 
 			// If output file is specified, use it
-			if(tree_out.length() > 0) tree_out = dir_out+tree_out;
-			// If output file is not specified, build it
-			else
-			{
-				// If tree output directory is not specified, build it
-				if(dir_out.length() == 0)
-				{
-					prefix = file.substr(0,file.find_last_of("/")+1);
-					dir_out = dir_in+prefix;
-				}
-				// If input prefix doesn't match, simply prepend output prefix to the file name
-				if(file.find(pre_in)>file.length()) suffix = ("_"+file.substr(file.find_last_of("/")+1,length-(file.find_last_of("/")+1)));
-				// If input prefix does match, switch prefixes
-				else suffix = file.substr(file.find_last_of("/")+1+pre_in.length(),length-(file.find_last_of("/")+1+pre_in.length()));
-				// Build output file name
-				tree_out = dir_out+pre_tree+suffix;
-			}
-
-			// If hist output file is specified, use it
-			if(hist_out.length() > 0) hist_out = dir_out+hist_out;
+			if(file_out.length() > 0) file_out = dir_out+file_out;
 			// If output file is not specified, build it
 			else
 			{
@@ -182,13 +186,11 @@ int main(int argc, char *argv[])
 				// If input prefix does match, switch prefixes
 				else suffix = file.substr(file.find_last_of("/")+1+pre_in.length(),length-(file.find_last_of("/")+1+pre_in.length()));
 				// Build output file name
-				hist_out = dir_out+pre_hist+suffix;
+				file_out = dir_out+pre_out+suffix;
 			}
 			
-			cout << "Output tree '" << tree_out << "' chosen" << endl;
-			cout << "Output hist '" << hist_out << "' chosen" << endl << endl;			
-			if(!ppi0->File(file_in.c_str(), tree_out.c_str(), hist_out.c_str())) 
-					cout << "ERROR: PPi0Example failed on file " << file_in << "!" << endl;
+			cout << "Output file '" << file_out << "' chosen" << endl << endl;
+			if(!ppi0->File(file_in.c_str(), file_out.c_str())) cout << "ERROR: PPi0Example failed on file " << file_in << "!" << endl;
 			files_found++;
 		}
 	}
@@ -196,10 +198,11 @@ int main(int argc, char *argv[])
 	else
 	{
 		cout << "Searching input directory for files matching input prefix" << endl;
-		cout << "Input prefix '" << pre_in << "' chosen" << endl << endl;
-			
+		cout << "Input prefix  '" << pre_in << "' chosen" << endl;
+		cout << "Output prefix '" << pre_out << "' chosen" << endl;
+		
 		// If output directory is not specified, use the input directory
-		if(dir_in.length()  == 0) dir_in  = "./"; // Search in current directory
+		if(dir_in.length()  == 0) dir_in = "./";
 		if(dir_out.length() == 0) dir_out = dir_in;
 
 		// Create list of files in input directory
@@ -226,15 +229,11 @@ int main(int argc, char *argv[])
 					file_in = dir_in+file;
 					// Build output file name
 					suffix = file.substr(pre_in.length(),length-pre_in.length());
-					tree_out = dir_out+pre_tree+suffix;					
-					hist_out = dir_out+pre_hist+suffix;
-					
+					file_out = dir_out+pre_out+suffix;					
+
 					files_found++;
 					// Run PPi0Example
-					
-					cout << "Output tree '" << tree_out << "' chosen" << endl;
-					cout << "Output hist '" << hist_out << "' chosen" << endl << endl;						
-					if(!ppi0->File(file_in.c_str(), tree_out.c_str(), hist_out.c_str())) 
+					if(!ppi0->File(file_in.c_str(), file_out.c_str())) 
 						cout << "ERROR: PPi0Example failed on file " << file_in << "!" << endl;
 
 				}
@@ -255,24 +254,8 @@ int main(int argc, char *argv[])
 	return 0;
 }
 
-PPi0Example::PPi0Example() :
-			nentries(0),
-			TC(0),
-			E(0),
-			time(0),
-			theta(0),
-			phi(0),
-			MissMass(0),
-			MissE(0)
+PPi0Example::PPi0Example() 
 { 
-
-	TC 		= new Int_t[maxentries];
-	E		= new Double_t[maxentries];
-	time	= new Double_t[maxentries];
-	theta	= new Double_t[maxentries];
-	phi		= new Double_t[maxentries];
-	MissMass= new Double_t[maxentries];
-	MissE	= new Double_t[maxentries];
 }
 
 PPi0Example::~PPi0Example()
@@ -282,339 +265,125 @@ PPi0Example::~PPi0Example()
 Bool_t	PPi0Example::Init(const char* configfile)
 {
 	// Initialise shared pdg database
-	pdgDB = TDatabasePDG::Instance();	
+	pdgDB = TDatabasePDG::Instance();
 
 	// Set by user in the future...
 	SetTarget(938);
 	
 	Double_t Prompt_low 	=  -20;
 	Double_t Prompt_high 	=   15;
-	Double_t Random_low1 	= -450;
-	Double_t Random_high1 	=  -50;
-	Double_t Random_low2 	=   50;
-	Double_t Random_high2 	=  500;
+	Double_t Random_low1 	= -100;
+	Double_t Random_high1 	=  -40;
+	Double_t Random_low2 	=   35;
+	Double_t Random_high2 	=   95;
 	
-	SetPromptWindow( Prompt_low,  Prompt_high);
+	SetPromptWindow(Prompt_low, Prompt_high);
 	SetRandomWindow1(Random_low1, Random_high1);
 	SetRandomWindow2(Random_low2, Random_high2);
-	SetPvRratio();	
-	
-	MMCut_low 		= 900;
-	MMCut_high 		= 1000;
-	theta_bin_size 	= 20;
-	phi_bin_size 	= 20;
-	
-	ReconstructPhysicsTree = kTRUE;
-	FillHistograms = kTRUE;
-	
+	SetPvRratio();
+
 	return kTRUE;
 }
 
-Bool_t	PPi0Example::File(const char* gfile, const char* tfile, const char* hfile)
+Bool_t	PPi0Example::File(const char* file_in, const char* file_out)
 {
-	if(ReconstructPhysicsTree == kTRUE)
-	{
-		OpenGoATFile(gfile, "READ");
-		OpenPhysTreeFile(tfile, "RECREATE");
+	OpenGoATFile(file_in, "READ");
+	OpenHistFile(file_out);
+	DefineHistograms();
 
-		cout << "Setting up tree files:" << endl;
-		if(!InitPhysicsTree(PhysTreeFile)) 	return kFALSE;
-		if(!OpenTreeParticles(GoATFile)) 	return kFALSE;
-		if(!OpenTreeTagger(GoATFile))		return kFALSE;
-		cout << endl;
+	cout << "Setting up tree files:" << endl;
+	if(!OpenTreeParticles(GoATFile)) 	return kFALSE;
+	if(!OpenTreeTagger(GoATFile))		return kFALSE;
+	cout << endl;
 
-		cout << "Detmining valid for analysis:" << endl;	
-		if(!FindValidGoATEvents())			return kFALSE;	
-
-	}
-
-	if(FillHistograms == kTRUE)
-	{
-		if(ReconstructPhysicsTree == kFALSE)
-		{
-			if(!OpenPhysTreeFile(tfile, "READ")) return kFALSE;
-		}
+	cout << "Detmining valid for analysis:" << endl;	
+	if(!FindValidGoATEvents())			return kFALSE;	
+	cout << endl;
 		
-		OpenHistFile(hfile,"RECREATE");
-		InitHistograms();
-	}
-	
 	Analyse();	
 	return kTRUE;
 }
 
 void	PPi0Example::Analyse()
 {
-	if(ReconstructPhysicsTree == kTRUE)
-	{	
-		cout << "Analysing ..." << endl;
-		TraverseGoATEntries();
-		WriteTrees();
-	}
 
-	if(FillHistograms == kTRUE)
-	{	
-
-		if(!OpenPhysicsTree()) cout << "whoops" << endl;
-		PostReconstruction();		
-		WriteHistograms();
-		CloseHistFile();
-	}
+	TraverseGoATEntries();
+	cout << "Total Pi0s found: " << N_pi0 << endl << endl;
 	
-	if((ReconstructPhysicsTree == kTRUE) || (FillHistograms == kTRUE))
-	{
-		ClosePhysTreeFile();
-	}
-		
+	PostReconstruction();		
+	WriteHistograms();
+	CloseHistFile();	
+
 }
 
 void	PPi0Example::Reconstruct()
 {
-	if(GetGoATEvent() % 100000 == 0) printf("Event: %d\n",GetGoATEvent());
+	if(GetGoATEvent() == 0) N_pi0 = 0;
+	else if(GetGoATEvent() % 100000 == 0) cout << "Event: "<< GetGoATEvent() << " Total Pi0s found: " << N_pi0 << endl;
 
-	nentries = 0;
+	// Fill timing histogram (all PDG matching pi0)
+	FillTimePDG(pdgDB->GetParticle("pi0")->PdgCode(),time_pi0);
+	
+	// Fill missing mass (all PDG matching pi0)
+	MissingMassPDG(pdgDB->GetParticle("pi0")->PdgCode(), MM_prompt_pi0, MM_random_pi0);
+
+	// Some neutral decays
 	for (Int_t i = 0; i < GoATTree_GetNParticles(); i++)
 	{
-		if (GoATTree_GetPDG(i) != pdgDB->GetParticle("pi0")->PdgCode()) continue;
+		// Count total pi0s
+		if(GoATTree_GetPDG(i) == pdgDB->GetParticle("pi0")->PdgCode()) 	N_pi0++;
+		
+		// Check PDG: Not pi0, continue
+		if (GoATTree_GetPDG(i) != pdgDB->GetParticle("pi0")->PdgCode()) continue; 
+		
+		// Check neutrality: Not neutral, continue
 		if (GoATTree_GetCharge(i) != 0) continue;
+		
+		// Check # of daughters: Not 2, continue
 		if (GoATTree_GetNDaughters(i) != 2) continue;
-
-		for (Int_t j = 0; j < GetNTagged(); j++)
-		{
-			TC[nentries] 		= GetTagged_ch(j);
-			E[nentries] 		= GetPhotonBeam_E(j);
-			time[nentries]		= GetTagged_t(j) - GoATTree_GetTime(i);
-			theta[nentries]		= GoATTree_GetTheta(i);
-			phi[nentries]		= GoATTree_GetPhi(i);
-			MissMass[nentries] 	= CalcMissingMass(i,j);
-			MissE[nentries]	 	= CalcMissingEnergy(i,j);
-			nentries++;
-		}
+		
+		// Fill missing mass for particle i
+		FillMissingMass(i, MM_prompt_pi0_n_2g, MM_prompt_pi0_n_2g);
 	}
-
-	treePhysics->Fill();
-
 }
 
 void  PPi0Example::PostReconstruction()
 {
-	Int_t N = (Int_t)treePhysics->GetEntries();
-	for (Int_t i=0;i<N;i++) {
+	cout << "Performing post reconstruction." << endl;
 
-		treePhysics->GetEntry(i);
-
-		for (Int_t j = 0; j < nentries; j ++)
-		{
-			time_pi0->Fill(time[j]);
-			
-			Prompt = IsPrompt(time[j]); 
-			Random = IsRandom(time[j]);
-			
-			if ((!Prompt) && (!Random)) continue;
-			if (Prompt) 
-			{
-				// Fill 1D histograms
-				TC_prompt->Fill(TC[j]);
-				E_prompt->Fill(E[j]);
-				theta_prompt->Fill(theta[j]);
-				phi_prompt->Fill(phi[j]);
-				MM_prompt_pi0->Fill(MissMass[j]);
-				ME_prompt_pi0->Fill(MissE[j]);	
-				
-				// Fill 3D histograms according to cuts
-				if((MissMass[j] >= MMCut_low) && (MissMass[j] <= MMCut_high))
-				{
-					TC_theta_phi_prompt->Fill(TC[j], theta[j], phi[j]);
-				}
-				
-			}
-			if (Random)
-			{
-				// Fill 1D histograms
-				TC_random->Fill(TC[j]);
-				E_random->Fill(E[j]);
-				theta_random->Fill(theta[j]);
-				phi_random->Fill(phi[j]);
-				MM_random_pi0->Fill(MissMass[j]);
-				ME_random_pi0->Fill(MissE[j]);	
-				
-				// Fill 3D histograms according to cuts
-				if((MissMass[j] >= MMCut_low) && (MissMass[j] <= MMCut_high))
-				{
-					TC_theta_phi_random->Fill(TC[j], theta[j], phi[j]);
-				}
-			}
-		}
-	}
-
-	RandomSubtraction(TC_prompt,TC_random, TC_sub);		
-	RandomSubtraction(E_prompt,E_random, E_sub);		
-	RandomSubtraction(theta_prompt,theta_random, theta_sub);		
-	RandomSubtraction(phi_prompt,phi_random, phi_sub);		
-	RandomSubtraction(MM_prompt_pi0,MM_random_pi0, MM_sub_pi0);		
-	RandomSubtraction(ME_prompt_pi0,ME_random_pi0, ME_sub_pi0);		
-
+	RandomSubtraction(MM_prompt_pi0,MM_random_pi0, MM_pi0);		
+	RandomSubtraction(MM_prompt_pi0_n_2g,MM_random_pi0_n_2g, MM_pi0_n_2g);	
+		
 	ShowTimeCuts(time_pi0, time_pi0_cuts);
 
 }
 
-Bool_t  PPi0Example::InitPhysicsTree(TFile* F)
-{
-	if(!F) return kFALSE;
-	F->cd();
-	
-	treePhysics = new TTree("treePhysics", "treePhysics");
-	if(!treePhysics) return kFALSE;
-	cout << "treePhysics created." << endl;
-	
-	treePhysics->Branch("nentries",	&nentries,	"nentries/I");
-	treePhysics->Branch("TC",		TC,			"TC[nentries]/I");
-	treePhysics->Branch("E",		E,			"E[nentries]/D");	
-	treePhysics->Branch("time",		time,		"time[nentries]/D");	
-	treePhysics->Branch("theta",	theta,		"theta[nentries]/D");
-	treePhysics->Branch("phi",		phi,		"phi[nentries]/D");
-	treePhysics->Branch("MissMass",	MissMass,	"MissMass[nentries]/D");
-	treePhysics->Branch("MissE",	MissE,		"MissE[nentries]/D");
-		
-	return kTRUE;
-}
-
-Bool_t  PPi0Example::OpenPhysicsTree(TFile* F)
-{
-	if(!F) {cout << "1" << endl;return kFALSE; }
-	
-	treePhysics = (TTree*)F->Get("treePhysics");
-	if(!treePhysics) {cout << "2" << endl;return kFALSE; }
-	cout << "treePhysics opened." << endl;
-	
-	treePhysics->SetBranchAddress("nentries",	&nentries);
-	treePhysics->SetBranchAddress("TC",			TC);
-	treePhysics->SetBranchAddress("E",			E);	
-	treePhysics->SetBranchAddress("time",		time);	
-	treePhysics->SetBranchAddress("theta",		theta);
-	treePhysics->SetBranchAddress("phi",		phi);
-	treePhysics->SetBranchAddress("MissMass",	MissMass);
-	treePhysics->SetBranchAddress("MissE",		MissE);
-		
-	return kTRUE;
-}
-
-Bool_t  PPi0Example::WriteTrees(TFile* F)
-{
-	if(!F) return kFALSE;
-	F->cd();
-	
-	if(treePhysics)				treePhysics->Write();
-	
-	return kTRUE;
-}
-
-void	PPi0Example::InitHistograms()
+void	PPi0Example::DefineHistograms()
 {
 	gROOT->cd();
 	
-	// 1-D histograms
+	time_pi0		= new TH1D("time_pi0",		"time_pi0",		1000,-500,500);
+	time_pi0_cuts	= new TH1D("time_pi0_cuts",	"time_pi0_cuts",1000,-500,500);
 
-	time_pi0		= new TH1D("time_pi0",		"time_pi0",		1200,-600,600);
-	time_pi0_cuts	= new TH1D("time_pi0_cuts",	"time_pi0_cuts",1200,-600,600);
-
-	TC_prompt 		= new TH1D("TC_prompt",		"TC_prompt",	352,0,352);
-	TC_random 		= new TH1D("TC_random",		"TC_random",	352,0,352);
-	TC_sub			= new TH1D("TC subtracted",	"TC subtracted",352,0,352);	
-
-	TC_prompt->Sumw2(); 
-	TC_random->Sumw2();
-	TC_sub->Sumw2();
-
-	E_prompt 		= new TH1D("E_prompt",		"E_prompt",		1600,0,1600);
-	E_random 		= new TH1D("E_random",		"E_random",		1600,0,1600);
-	E_sub			= new TH1D("E subtracted",	"E subtracted",	1600,0,1600);
-
-	E_prompt->Sumw2(); 
-	E_random->Sumw2();
-	E_sub->Sumw2();
+	MM_prompt_pi0 	= new TH1D("MM_prompt_pi0",	"MM_prompt_pi0",1500,0,1500);
+	MM_random_pi0 	= new TH1D("MM_random_pi0",	"MM_random_pi0",1500,0,1500);
+	MM_pi0			= new TH1D("MM_pi0",		"MM_pi0",		1500,0,1500);
 	
-	theta_prompt 	= new TH1D("theta_prompt",		"theta_prompt",		180,0,180);	
-	theta_random 	= new TH1D("theta_random",		"theta_random",		180,0,180);
-	theta_sub		= new TH1D("theta_subtracted",	"theta_subtracted",	180,0,180);
-	
-	theta_prompt->Sumw2(); 
-	theta_random->Sumw2();
-	theta_sub->Sumw2();	
-	
-	phi_prompt 		= new TH1D("phi_prompt",		"phi_prompt",		360,-180,180);
-	phi_random 		= new TH1D("phi_random",		"phi_random",		360,-180,180);
-	phi_sub				= new TH1D("phi_subtracted","phi_subtracted",	360,-180,180);	
-
-	phi_prompt->Sumw2(); 
-	phi_random->Sumw2();
-	phi_sub->Sumw2();	
-
-	MM_prompt_pi0 	= new TH1D("MM_prompt_pi0",		"MM_prompt_pi0",	1500,0,1500); 
-	MM_random_pi0 	= new TH1D("MM_random_pi0",		"MM_random_pi0",	1500,0,1500); 
-	MM_sub_pi0		= new TH1D("MM_subtracted_pi0",	"MM_subtracted_pi0",1500,0,1500); 
-
-	MM_prompt_pi0->Sumw2(); 
-	MM_random_pi0->Sumw2();
-	MM_sub_pi0->Sumw2();
-
-	ME_prompt_pi0 	= new TH1D("ME_prompt_pi0",		"ME_prompt_pi0",	1500,0,1500); 
-	ME_random_pi0 	= new TH1D("ME_random_pi0",		"ME_random_pi0",	1500,0,1500); 
-	ME_sub_pi0		= new TH1D("ME_subtracted_pi0",	"ME_subtracted_pi0",1500,0,1500); 
-
-	ME_prompt_pi0->Sumw2(); 
-	ME_random_pi0->Sumw2();
-	ME_sub_pi0->Sumw2();
-
-
-	// 3D histograms (for analysis)
-	Int_t theta_bins = int(180/theta_bin_size) + 1;
-	Double_t max_theta = (theta_bins)*theta_bin_size;
-
-	Int_t phi_bins 	 = int(360/phi_bin_size) + 1;
-	Double_t max_phi = (phi_bins)*phi_bin_size;	
-
-
-	TC_theta_phi_prompt	= new TH3D("TC_theta_phi_prompt","TC_theta_phi_prompt",	352,0,352, theta_bins,0,max_theta, 	phi_bins,-180,max_phi);
-	TC_theta_phi_random = new TH3D("TC_theta_phi_random","TC_theta_phi_random",	352,0,352, theta_bins,0,max_theta, 	phi_bins,-180,max_phi);
-
-	TC_theta_phi_prompt->Sumw2(); 
-	TC_theta_phi_random->Sumw2();
-
-/*	E_theta_phi_prompt = new TH3D("E_theta_phi_prompt","E_theta_phi_prompt", 160,0,1600 	 theta_bins,0,180, 	phi_bins,-180,180);
-	E_theta_phi_random = new TH3D("E_theta_phi_random","E_theta_phi_random", 160,0,1600, 	 theta_bins,0,180, 	phi_bins,-180,180);
-
-	E_theta_phi_prompt->Sumw2(); 
-	E_theta_phi_random->Sumw2();*/
+	MM_prompt_pi0_n_2g = new TH1D("MM_prompt_pi0_n_2g",	"MM_prompt_pi0_n_2g",1500,0,1500);
+	MM_random_pi0_n_2g = new TH1D("MM_random_pi0_n_2g",	"MM_random_pi0_n_2g",1500,0,1500);
+	MM_pi0_n_2g		  = new TH1D("MM_pi0_n_2g",		 	"MM_pi0_n_2g",	   	 1500,0,1500);		
 }
 
-Bool_t 	PPi0Example::WriteHistograms(TFile* hfile)
+Bool_t 	PPi0Example::WriteHistograms(TFile* pfile)
 {
-	if(!hfile) return kFALSE;
-	hfile->cd();
+	cout << "Writing histograms." << endl;
+		
+	if(!pfile) return kFALSE;
+	pfile->cd();
 
 	gROOT->GetList()->Write();
 	gROOT->GetList()->Delete();
 		
-	return kTRUE;
-}
-
-Bool_t	PPi0Example::OpenPhysTreeFile(const char* tfile, Option_t* option)
-{
-	PhysTreeFile = new TFile(tfile, option);
-	if(!PhysTreeFile) return kFALSE;
-    if(PhysTreeFile->IsZombie()) return kFALSE;
-    
-	cout << "PhysTreeFile " << tfile << " opened (option = "<< option << ")." << endl;
-	
-	return kTRUE;
-}
-
-Bool_t 	PPi0Example::ClosePhysTreeFile()
-{
-	if(!PhysTreeFile) return kFALSE;
-	PhysTreeFile->Close();
-	
 	return kTRUE;
 }
 
