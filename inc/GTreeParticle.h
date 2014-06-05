84bb3599910ac31a73fc5e4516a6b03b82910ce0
Patrik Ott <ott@kph.uni-mainz.de>
2014-06-05 11:43:00 +0200

    Changed Structure of GTreeParticle. No indices to RawEvents. Parameters time, clusterSize, Apparatus, d_e, WC0_E and WC1_E are added to tree

--- inc/GTreeParticle.h
+++ inc/GTreeParticle.h
@@ -7,7 +7,7 @@
 
 #include "GTree.h"
 
-#define GTreeParticle_MaxEntries  128
+#define GTreeParticle_MaxEntries  64
 
 class   GTreeMeson;
 
@@ -16,7 +16,13 @@ class  GTreeParticle    : public GTree
 private:
     UInt_t              nParticles;
     TClonesArray*       particles;	// reconstructed
-    Int_t               daughters[GTreeParticle_MaxEntries];  // nDaughters in GTreeMeson
+    UChar_t             Apparatus[GTreeParticle_MaxEntries];
+    Double_t            time[GTreeParticle_MaxEntries];
+    UChar_t             clusterSize[GTreeParticle_MaxEntries];
+    //Charged detector energies
+    Double_t            d_E[GTreeParticle_MaxEntries];
+    Double_t            WC0_E[GTreeParticle_MaxEntries];
+    Double_t            WC1_E[GTreeParticle_MaxEntries];
 
 
 protected:
@@ -27,21 +33,34 @@ public:
     GTreeParticle(GTreeManager *Manager, const TString& _Name);
     virtual ~GTreeParticle();
 
-    inline  void            AddParticle(const TLorentzVector& vec, const Int_t _RawIndex = -1);
+    inline  void            AddParticle(const TLorentzVector& vec, const UChar_t _Apparatus = 0, const Double_t _d_E = 0, const Double_t _WC0_E = 0, const Double_t _WC1_E = 0, const Double_t _Time = 0, const UChar_t _ClusterSize = 0);
     virtual void            Clear()     {nParticles = 0; particles->Clear();}
-            Int_t           GetDaughterIndex(const Int_t index) const       {return daughters[index];}
-            UInt_t          GetNParticles()    const       {return nParticles;}
+            UChar_t         GetApparatus(const Int_t index)     const	{return Apparatus[index];}
+            UChar_t         GetClusterSize(const Int_t index)   const 	{return clusterSize[index];}
+            Double_t        Get_dE(const Int_t index)           const	{return d_E[index];}
+            UInt_t          GetNParticles()                     const	{return nParticles;}
+    const	Double_t*       GetTime()                           const	{return time;}
+            Double_t        GetTime(const Int_t index)          const	{return time[index];}
+    const	Double_t*       GetWC0_E()                          const	{return WC0_E;}
+            Double_t        GetWC0_E(const Int_t index)         const	{return WC0_E[index];}
+    const	Double_t*       GetWC1_E()                          const	{return WC1_E;}
+            Double_t        GetWC1_E(const Int_t index)         const	{return WC1_E[index];}
             TLorentzVector& Particle(const Int_t particle) {return *((TLorentzVector*)particles->At(particle));}
     const   TLorentzVector& Particle(const Int_t particle) const {return *((TLorentzVector*)particles->At(particle));}
-            void            RemoveParticle(const Int_t index)   {particles->RemoveAt(index); particles->Compress();}
+            void            RemoveParticle(const Int_t index)   {particles->RemoveAt(index); particles->Compress();}   
 
 
     friend  class GTreeMeson;
 };
 
-void    GTreeParticle::AddParticle(const TLorentzVector& vec, const Int_t _RawIndex)
+void    GTreeParticle::AddParticle(const TLorentzVector& vec, const UChar_t _Apparatus, const Double_t _d_E, const Double_t _WC0_E, const Double_t _WC1_E, const Double_t _Time, const UChar_t _ClusterSize)
 {
-    daughters[nParticles]   = _RawIndex;
+    Apparatus[nParticles]   = _Apparatus;
+    time[nParticles]        = _Time;
+    clusterSize[nParticles] = _ClusterSize;
+    d_E[nParticles]         = _d_E;
+    WC0_E[nParticles]       = _WC0_E;
+    WC0_E[nParticles]       = _WC1_E;
     new((*particles)[nParticles]) TLorentzVector(vec);
     nParticles++;
 }
--- src/GTreeParticle.cc
+++ src/GTreeParticle.cc
@@ -8,25 +8,44 @@ using namespace std;
 GTreeParticle::GTreeParticle(GTreeManager *Manager, const TString& _Name)    :
     GTree(Manager,_Name),
     nParticles(0),
-    particles(new TClonesArray("TLorentzVector", 32000))
+    particles(new TClonesArray("TLorentzVector", 32))
 {
+    for(int i=0; i<GTreeParticle_MaxEntries; i++)
+    {
+        Apparatus[i]    = 0;
+        time[i]         = 0;
+        clusterSize[i]  = 0;
+        d_E[i]          = 0;
+        WC0_E[i]        = 0;
+        WC1_E[i]        = 0;
+    }
 }
 
 GTreeParticle::~GTreeParticle()
 {
+    if (particles)  delete particles;
 }
 
 void    GTreeParticle::SetBranchAdresses()
 {
     tree_in->SetBranchAddress("nParticles",&nParticles);
     tree_in->SetBranchAddress("particles.", &particles);
-    tree_in->SetBranchAddress("rawIndex",daughters);
+    tree_in->SetBranchAddress("Apparatus",&Apparatus);
+    tree_in->SetBranchAddress("time",&time);
+    tree_in->SetBranchAddress("clusterSize",&clusterSize);
+    tree_in->SetBranchAddress("d_E",&d_E);
+    tree_in->SetBranchAddress("WC0_E",&WC0_E);
+    tree_in->SetBranchAddress("WC1_E",&WC1_E);
 }
 
 void    GTreeParticle::SetBranches()
 {
-    tree_out->Branch("nParticles",&nParticles,"nParticles/i");
+    tree_out->Branch("nParticles",&nParticles, "nParticles/i");
     tree_out->Branch("particles.", &particles, 32000, 0);
-    tree_out->Branch("rawIndex",daughters,"rawIndex[nParticles]/I");
+    tree_out->Branch("Apparatus",&Apparatus, "Apparatus[nParticles]/b");
+    tree_out->Branch("time",&time, "time[nParticles]/D");
+    tree_out->Branch("clusterSize",&clusterSize, "clusterSize[nParticles]/b");
+    tree_out->Branch("d_E",&d_E, "d_E[nParticles]/D");
+    tree_out->Branch("WC0_E",&WC0_E, "WC0_E[nParticles]/D");
+    tree_out->Branch("WC1_E",&WC1_E, "WC1_E[nParticles]/D");
 }
-
