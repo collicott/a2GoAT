f81aaedd8edce206dcf2026b6b861c65691282eb
Patrik Ott <ott@kph.uni-mainz.de>
2014-06-16 13:32:19 +0200

    clean up. deleted old comments.

--- inc/GTreeMeson.h
+++ inc/GTreeMeson.h
@@ -26,7 +26,6 @@ private:
     TClonesArray*       particles;
     UChar_t             nPhotons[GTreeMeson_MaxEntries];
     UChar_t             nChargedPi[GTreeMeson_MaxEntries];
-    //Int_t               pdg[GTreeMeson_MaxSubEntries * GTreeMeson_MaxSubEntries];
     TClonesArray*       subParticles;
 
 protected:
@@ -38,14 +37,8 @@ public:
     virtual ~GTreeMeson();
 
             void            AddParticle(const TLorentzVector& subParticle0, const TLorentzVector& subParticle1, const Int_t pdg0, const Int_t pdg1);
-            //void            AddParticle(const Int_t nSubParticles, TLorentzVector** subParticle_list, const Int_t* pdg_list);
             void            AddParticle(const Int_t nSubPhotons, TLorentzVector** subPhotons_list, const Int_t nSubChargedPi, TLorentzVector** subChargedPi_list);
-    /*inline  void            AddParticle( const TLorentzVector& _SubParticle0, const TLorentzVector& _SubParticle1);
-    inline  void            AddParticle(const GTreeMeson_DecayType type, const TLorentzVector& _SubParticle0, const TLorentzVector& _SubParticle1, const TLorentzVector& _SubParticle2);
-    inline  void            AddParticle(const GTreeMeson_DecayType type, const TLorentzVector& _SubParticle0, const TLorentzVector& _SubParticle1, const TLorentzVector& _SubParticle2, const TLorentzVector& _SubParticle3);
-    inline  void            AddParticle(const GTreeMeson_DecayType type, const TLorentzVector& _SubParticle0, const TLorentzVector& _SubParticle1, const TLorentzVector& _SubParticle2, const TLorentzVector& _SubParticle3, const TLorentzVector& _SubParticle4);
-    inline  void            AddParticle(const GTreeMeson_DecayType type, const TLorentzVector& _SubParticle0, const TLorentzVector& _SubParticle1, const TLorentzVector& _SubParticle2, const TLorentzVector& _SubParticle3, const TLorentzVector& _SubParticle4, const TLorentzVector& _SubParticle5);*/
-    virtual void            Clear()     {nParticles = 0; particles->Clear(); subParticles->Clear();}// subParticles[1]->Clear(); subParticles[2]->Clear(); subParticles[3]->Clear(); subParticles[4]->Clear(); subParticles[5]->Clear();}
+    virtual void            Clear()     {nParticles = 0; particles->Clear(); subParticles->Clear();}
             UInt_t          GetNParticles()                const	{return nParticles;}
             TLorentzVector& Meson(const Int_t particle)             {return *((TLorentzVector*)particles->At(particle));}
     const   TLorentzVector& Meson(const Int_t particle) const       {return *((TLorentzVector*)particles->At(particle));}
@@ -56,79 +49,5 @@ public:
 };
 
 
-/*void    GTreeMeson::AddParticle(const TLorentzVector& _SubParticle0, const TLorentzVector& _SubParticle1)
-{
-    nPhotons[nParticles]   = 0;
-    new((*particles)[nParticles]) TLorentzVector(_SubParticle0 + _SubParticle1);
-    //new((*subParticles[0])[nParticles]) TLorentzVector(_SubParticle0);
-    //new((*subParticles[1])[nParticles]) TLorentzVector(_SubParticle1);
-    nParticles++;
-}/*
-void    GTreeMeson::AddParticle(const GTreeMeson_DecayType type, const TLorentzVector& _SubParticle0, const TLorentzVector& _SubParticle1, const TLorentzVector& _SubParticle2)
-{
-    if(type != Decay_2ChargedPi1Gamma)
-    {
-        cout << "Given Wrong type or wrong # of subParticles in GTreeMeson::AddParticle" << endl;
-        return;
-    }
-
-    decayType[nParticles]   = type;
-    new((*particles)[nParticles]) TLorentzVector(_SubParticle0 + _SubParticle1 + _SubParticle2);
-    new((*subParticles[0])[nParticles]) TLorentzVector(_SubParticle0);
-    new((*subParticles[1])[nParticles]) TLorentzVector(_SubParticle1);
-    new((*subParticles[2])[nParticles]) TLorentzVector(_SubParticle2);
-    nParticles++;
-}
-void    GTreeMeson::AddParticle(const GTreeMeson_DecayType type, const TLorentzVector& _SubParticle0, const TLorentzVector& _SubParticle1, const TLorentzVector& _SubParticle2, const TLorentzVector& _SubParticle3)
-{
-    if(type != Decay_1Pi02ChargedPi)
-    {
-        cout << "Given Wrong type or wrong # of subParticles in GTreeMeson::AddParticle" << endl;
-        return;
-    }
-
-    decayType[nParticles]   = type;
-    new((*particles)[nParticles]) TLorentzVector(_SubParticle0 + _SubParticle1 + _SubParticle2 + _SubParticle3);
-    new((*subParticles[0])[nParticles]) TLorentzVector(_SubParticle0);
-    new((*subParticles[1])[nParticles]) TLorentzVector(_SubParticle1);
-    new((*subParticles[2])[nParticles]) TLorentzVector(_SubParticle2);
-    new((*subParticles[3])[nParticles]) TLorentzVector(_SubParticle3);
-    nParticles++;
-}
-void    GTreeMeson::AddParticle(const GTreeMeson_DecayType type, const TLorentzVector& _SubParticle0, const TLorentzVector& _SubParticle1, const TLorentzVector& _SubParticle2, const TLorentzVector& _SubParticle3, const TLorentzVector& _SubParticle4)
-{
-    if(type != Decay_2Pi01ChargedPi && type != Decay_1Pi02ChargedPi1Gamma)
-    {
-        cout << "Given Wrong type or wrong # of subParticles in GTreeMeson::AddParticle" << endl;
-        return;
-    }
-
-    decayType[nParticles]   = type;
-    new((*particles)[nParticles]) TLorentzVector(_SubParticle0 + _SubParticle1 + _SubParticle2 + _SubParticle3 + _SubParticle4);
-    new((*subParticles[0])[nParticles]) TLorentzVector(_SubParticle0);
-    new((*subParticles[1])[nParticles]) TLorentzVector(_SubParticle1);
-    new((*subParticles[2])[nParticles]) TLorentzVector(_SubParticle2);
-    new((*subParticles[3])[nParticles]) TLorentzVector(_SubParticle3);
-    new((*subParticles[4])[nParticles]) TLorentzVector(_SubParticle4);
-    nParticles++;
-}
-void    GTreeMeson::AddParticle(const GTreeMeson_DecayType type, const TLorentzVector& _SubParticle0, const TLorentzVector& _SubParticle1, const TLorentzVector& _SubParticle2, const TLorentzVector& _SubParticle3, const TLorentzVector& _SubParticle4, const TLorentzVector& _SubParticle5)
-{
-    if(type != Decay_3Pi0 && type != Decay_2Pi01Eta)
-    {
-        cout << "Given Wrong type or wrong # of subParticles in GTreeMeson::AddParticle" << endl;
-        return;
-    }
-
-    decayType[nParticles]   = type;
-    new((*particles)[nParticles]) TLorentzVector(_SubParticle0 + _SubParticle1 + _SubParticle2 + _SubParticle3 + _SubParticle4 + _SubParticle5);
-    new((*subParticles[0])[nParticles]) TLorentzVector(_SubParticle0);
-    new((*subParticles[1])[nParticles]) TLorentzVector(_SubParticle1);
-    new((*subParticles[2])[nParticles]) TLorentzVector(_SubParticle2);
-    new((*subParticles[3])[nParticles]) TLorentzVector(_SubParticle3);
-    new((*subParticles[4])[nParticles]) TLorentzVector(_SubParticle4);
-    new((*subParticles[5])[nParticles]) TLorentzVector(_SubParticle5);
-    nParticles++;
-}*/
 
 #endif
--- src/GTreeMeson.cc
+++ src/GTreeMeson.cc
@@ -8,27 +8,9 @@ GTreeMeson::GTreeMeson(GTreeManager *Manager, const TString& _Name)    :
     particles(new TClonesArray("TLorentzVector", 32))
 {
     subParticles = new TClonesArray("TClonesArray", 32);
-    /*subParticles[1] = new TClonesArray("TLorentzVector", 32);
-    subParticles[2] = new TClonesArray("TLorentzVector", 32);
-    subParticles[3] = new TClonesArray("TLorentzVector", 32);
-    subParticles[4] = new TClonesArray("TLorentzVector", 32);
-    subParticles[5] = new TClonesArray("TLorentzVector", 32);*/
-
-    /*pdg[0]  = new Int_t[GTreeMeson_MaxEntries];
-    pdg[1]  = new Int_t[GTreeMeson_MaxEntries];
-    pdg[2]  = new Int_t[GTreeMeson_MaxEntries];
-    pdg[3]  = new Int_t[GTreeMeson_MaxEntries];
-    pdg[4]  = new Int_t[GTreeMeson_MaxEntries];
-    pdg[5]  = new Int_t[GTreeMeson_MaxEntries];*/
 
     for(int i=0; i<GTreeMeson_MaxEntries; i++)
     {
-        /*pdg[0][i]         = 0;
-        pdg[1][i]         = 0;
-        pdg[2][i]         = 0;
-        pdg[3][i]         = 0;
-        pdg[4][i]         = 0;
-        pdg[5][i]         = 0;*/
         nPhotons[i]    = 0;
         nChargedPi[i]  = 0;
     }
@@ -38,17 +20,6 @@ GTreeMeson::~GTreeMeson()
 {
     if (particles)        delete particles;
     if (subParticles)  delete subParticles;
-    /*if (subParticles[1])  delete subParticles[1];
-    if (subParticles[2])  delete subParticles[2];
-    if (subParticles[3])  delete subParticles[3];
-    if (subParticles[4])  delete subParticles[4];
-    if (subParticles[5])  delete subParticles[5];
-    if (pdg[0])  delete pdg[0];
-    if (pdg[1])  delete pdg[1];
-    if (pdg[2])  delete pdg[2];
-    if (pdg[3])  delete pdg[3];
-    if (pdg[4])  delete pdg[4];
-    if (pdg[5])  delete pdg[5];*/
 }
 
 void    GTreeMeson::SetBranchAdresses()
@@ -60,17 +31,6 @@ void    GTreeMeson::SetBranchAdresses()
     tree_in->SetBranchAddress("nPhotons",&nPhotons);
     tree_in->SetBranchAddress("nChargedPi",&nChargedPi);
     tree_in->SetBranchAddress("subParticles.",&subParticles);
-    /*tree_in->SetBranchAddress("subParticle1.",&subParticles[1]);
-    tree_in->SetBranchAddress("subParticle2.",&subParticles[2]);
-    tree_in->SetBranchAddress("subParticle3.",&subParticles[3]);
-    tree_in->SetBranchAddress("subParticle4.",&subParticles[4]);
-    tree_in->SetBranchAddress("subParticle5.",&subParticles[5]);*/
-    /*tree_in->SetBranchAddress("pdgSubParticle0",pdg[0]);
-    tree_in->SetBranchAddress("pdgSubParticle1",pdg[1]);
-    tree_in->SetBranchAddress("pdgSubParticle2",pdg[2]);
-    tree_in->SetBranchAddress("pdgSubParticle3",pdg[3]);
-    tree_in->SetBranchAddress("pdgSubParticle4",pdg[4]);
-    tree_in->SetBranchAddress("pdgSubParticle5",pdg[5]);*/
 }
 
 void    GTreeMeson::SetBranches()
@@ -80,17 +40,6 @@ void    GTreeMeson::SetBranches()
     tree_out->Branch("nPhotons",nPhotons,"nParticles/b");
     tree_out->Branch("nChargedPi",nChargedPi,"nParticles/b");
     tree_out->Branch("subParticles.", &subParticles, 32, 0);
-    /*tree_out->Branch("subParticle1.", &subParticles[1], 32, 0);
-    tree_out->Branch("subParticle2.", &subParticles[2], 32, 0);
-    tree_out->Branch("subParticle3.", &subParticles[3], 32, 0);
-    tree_out->Branch("subParticle4.", &subParticles[4], 32, 0);
-    tree_out->Branch("subParticle5.", &subParticles[5], 32, 0);
-    tree_out->Branch("pdgSubParticle0",pdg[0],"pdgSubParticle0[nParticles]/i");
-    tree_out->Branch("pdgSubParticle1",pdg[1],"pdgSubParticle1[nParticles]/i");
-    tree_out->Branch("pdgSubParticle2",pdg[2],"pdgSubParticle2[nParticles]/i");
-    tree_out->Branch("pdgSubParticle3",pdg[3],"pdgSubParticle3[nParticles]/i");
-    tree_out->Branch("pdgSubParticle4",pdg[4],"pdgSubParticle4[nParticles]/i");
-    tree_out->Branch("pdgSubParticle5",pdg[5],"pdgSubParticle5[nParticles]/i");*/
 }
 
 void    GTreeMeson::AddParticle(const TLorentzVector& subParticle0, const TLorentzVector& subParticle1, const Int_t pdg0, const Int_t pdg1)
@@ -132,36 +81,6 @@ void    GTreeMeson::AddParticle(const TLorentzVector& subParticle0, const TLoren
     new((*particles)[nParticles]) TLorentzVector(subParticle0 + subParticle1);
     nParticles++;
 }
-
-/*void    GTreeMeson::AddParticle(const Int_t nSubParticles, TLorentzVector** subParticle_list, const Int_t* pdg_list)
-{
-    new((*subParticles)[nParticles]) TClonesArray("TLorentzVector", 32);
-
-    nPhotons[nParticles]    = 0;
-    nChargedPi[nParticles]  = 0;
-    TLorentzVector  sum;
-    for(int i=0; i<nSubParticles; i++)
-    {
-        if(pdg_list[i] == manager->pdgDB->GetParticle("gamma")->PdgCode())
-        {
-            nPhotons[nParticles]++;
-            //pdg[i][nParticles]  = manager->pdgDB->GetParticle("gamma")->PdgCode();
-        }
-        else
-        {
-            nChargedPi[nParticles]++;
-            //pdg[i][nParticles]  = manager->pdgDB->GetParticle("pi+")->PdgCode();
-        }
-        sum += *subParticle_list[i];
-        new((*((TClonesArray*)subParticles->At(nParticles)))[i]) TLorentzVector(*subParticle_list[i]);
-    }
-    for(int i=nSubParticles; i<GTreeMeson_MaxSubEntries; i++)
-    {
-        //pdg[i][nParticles]  = 0;
-    }
-    new((*particles)[nParticles]) TLorentzVector(sum);
-    nParticles++;
-}*/
 
 void    GTreeMeson::AddParticle(const Int_t nSubPhotons, TLorentzVector** subPhotons_list, const Int_t nSubChargedPi, TLorentzVector** subChargedPi_list)
 {
