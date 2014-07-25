e4ce10e8c844019fcb2f83801bd4a6b7f6d9d783
Patrik Ott <ott@kph.uni-mainz.de>
2014-07-25 18:31:29 +0200

    added GH1-example for testing.

---
+++ inc/GH1Example.h
@@ -0,0 +1,30 @@
+#ifndef __GH1Example_h__
+#define __GH1Example_h__
+
+#include <iostream>
+#include <fstream>
+#include <cstdio>
+#include <string> 
+
+#include "GTreeManager.h"
+#include "GH1.h"
+
+class	GH1Example  : public GTreeManager
+{
+private:
+    GH1     test;
+
+protected:
+    virtual Bool_t  Start();
+
+    virtual void    ProcessEvent();
+    virtual void	ProcessScalerRead();
+			
+public:
+    GH1Example();
+    virtual ~GH1Example();
+
+    //virtual Bool_t	Init(const char* configfile);
+
+};
+#endif
