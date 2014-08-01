9adda4b40e6e32bf982306f44d0a112f136259eb
Cristina Collicott <collicott@dal.ca>
2014-08-01 10:54:42 +0200

    PPhysics library now based off GH1 histograms

--- inc/PPhysics.h
+++ inc/PPhysics.h
@@ -9,6 +9,7 @@
 #include <TH3.h>
 
 #include "GTreeManager.h"
+#include "GH1.h"
 #include "GConfigFile.h"
 
 
@@ -47,43 +48,22 @@ public:
 	virtual void	Reconstruct();
     virtual Bool_t	Write();
 
-    void 	MissingMassPDG(const GTreeParticle &tree, TH1* Hprompt, TH1* Hrandom);
-	void 	MissingMassPDG(Int_t pdg, TH1* Hprompt, TH1* Hrandom);
-    Bool_t	FillMissingMass(const GTreeParticle &tree, Int_t particle_index, TH1* Hprompt, TH1* Hrandom);
-    Bool_t 	FillMissingMassPair(const GTreeParticle &tree, Int_t particle_index, Int_t tagger_index, TH1* Hprompt, TH1* Hrandom);
+	void	FillMissingMass(const GTreeParticle& tree, GH1* gHist);
+	void	FillMissingMass(const GTreeParticle& tree, Int_t particle_index, GH1* gHist);
+	void 	FillMissingMass(const GTreeParticle& tree, Int_t particle_index, Int_t tagger_index, GH1* gHist);
+
     Double_t CalcMissingMass(const GTreeParticle &tree, Int_t particle_index, Int_t tagger_index);
     Double_t CalcMissingEnergy(const GTreeParticle &tree, Int_t particle_index, Int_t tagger_index);
     TLorentzVector CalcMissingP4(const GTreeParticle &tree, Int_t particle_index, Int_t tagger_index);
-			
-    void 	FillTimePDG(const GTreeParticle& tree, TH1* Htime);
-	void	ShowTimeCuts(TH1* timeH, TH1* cutsH, Double_t t1, Double_t t2, Double_t t3, Double_t t4, Double_t t5, Double_t t6);
-	void	ShowTimeCuts(TH1* timeH, TH1* cutsH) {ShowTimeCuts(timeH, cutsH, Random_low1, Random_high1, Prompt_low, Prompt_high, Random_low2, Random_high2);}
-	
-	Bool_t 	IsPrompt(Double_t time, Double_t t_low, Double_t t_high);
-	Bool_t 	IsPrompt(Double_t time) {return IsPrompt(time, Prompt_low, Prompt_high);}
-	Bool_t 	IsRandom(Double_t time, Double_t t_low1, Double_t t_high1, Double_t t_low2, Double_t t_high2 );
-	Bool_t 	IsRandom(Double_t time) {return IsRandom(time, Random_low1, Random_high1, Random_low2, Random_high2);}
-	
+
+	void 	FillTime(const GTreeParticle& tree, GH1* gHist);
+	void 	FillTime(const GTreeParticle& tree, Int_t particle_index, GH1* gHist);
+
+	void 	FillMass(const GTreeParticle& tree, GH1* gHist);
+	void 	FillMass(const GTreeParticle& tree, Int_t particle_index, GH1* gHist);
+				
 	void	SetTarget(Double_t mass) {target = TLorentzVector(0.,0.,0.,mass);}
 	TLorentzVector GetTarget() {return target;}
 	
-	void	SetPromptWindow( Double_t t1, Double_t t2) {Prompt_low  = t1; Prompt_high  = t2;}
-	void	SetRandomWindow1(Double_t t1, Double_t t2) {Random_low1 = t1; Random_high1 = t2;}
-	void	SetRandomWindow2(Double_t t1, Double_t t2) {Random_low2 = t1; Random_high2 = t2;}
-	void 	SetPvRratio(Double_t value) {PvR_ratio = value;}
-	void 	SetPvRratio() {PvR_ratio = (Prompt_high - Prompt_low)/((Random_high1 - Random_low1) + (Random_high2 - Random_low2));}
-	
-	void	RandomSubtraction(TH1* prompt, TH1* random, TH1* sub, Double_t ratio);
-	void	RandomSubtraction(TH1* prompt, TH1* random, TH1* sub) {RandomSubtraction(prompt,random,sub,PvR_ratio);}
-	
-	void	RandomSubtraction(TH3* prompt, TH3* random, TH3* sub, Double_t ratio);
-	void	RandomSubtraction(TH3* prompt, TH3* random, TH3* sub) {RandomSubtraction(prompt,random,sub,PvR_ratio);}
-
-	TFile* 	HistFile;
-	Bool_t 	OpenHistFile(const char* pfile, Option_t* option);
-	Bool_t 	OpenHistFile(const char* pfile) { return OpenHistFile(pfile,"RECREATE");}
-	Bool_t	CloseHistFile();	
-
-		
 };
 #endif
--- src/PPhysics.cc
+++ src/PPhysics.cc
@@ -20,43 +20,36 @@ void	PPhysics::Reconstruct()
 {
 }
 
-void PPhysics::MissingMassPDG(const GTreeParticle& tree, TH1* Hprompt, TH1* Hrandom)
+void PPhysics::FillMissingMass(const GTreeParticle& tree, GH1* gHist)
 {
-    for (Int_t i = 0; i < tree.GetNParticles(); i++)
-    {
-        particle = tree.Particle(i);
-		
-        for (Int_t j = 0; j < tagger->GetNTagged(); j++)
+	for (Int_t i = 0; i < tree.GetNParticles(); i++)
+	{
+		for (Int_t j = 0; j < tagger->GetNTagged(); j++)
 		{
-            FillMissingMassPair(tree, i, j, Hprompt, Hrandom);
+			FillMissingMass(tree, i, j, gHist);
 		}
 	}
-}	
+}
 
-Bool_t PPhysics::FillMissingMass(const GTreeParticle& tree, Int_t particle_index, TH1* Hprompt, TH1* Hrandom)
+void PPhysics::FillMissingMass(const GTreeParticle& tree, Int_t particle_index, GH1* gHist)
 {
     for (Int_t i = 0; i < tagger->GetNTagged(); i++)
 	{
-        FillMissingMassPair(tree, particle_index, i, Hprompt, Hrandom);
+        FillMissingMass(tree, particle_index, i, gHist);
 	}
-	return kTRUE;	
 }
 
-Bool_t PPhysics::FillMissingMassPair(const GTreeParticle& tree, Int_t particle_index, Int_t tagger_index, TH1* Hprompt, TH1* Hrandom)
+void PPhysics::FillMissingMass(const GTreeParticle& tree, Int_t particle_index, Int_t tagger_index, GH1* gHist)
 {
+    // calc particle time diff
     time = tagger->GetTagged_t(tagger_index) - tree.GetTime(particle_index);
-
-	Prompt = IsPrompt(time);
-	Random = IsRandom(time);
-	
-	if ((!Prompt) && (!Random)) return kFALSE;
-
+    
+    // calc missing p4
     missingp4 = CalcMissingP4(tree, particle_index,tagger_index);
 
-	if (Prompt) Hprompt->Fill(missingp4.M());
-	if (Random) Hrandom->Fill(missingp4.M());						
+	// Fill GH1
+	gHist->Fill(missingp4.M(),time);					
 
-	return kTRUE;
 }
 
 Double_t PPhysics::CalcMissingMass(const GTreeParticle& tree, Int_t particle_index, Int_t tagger_index)
@@ -82,96 +75,42 @@ TLorentzVector PPhysics::CalcMissingP4(const GTreeParticle& tree, Int_t particle
 	return missingp4;
 }
 
-void PPhysics::FillTimePDG(const GTreeParticle& tree, TH1* Htime)
+void PPhysics::FillTime(const GTreeParticle& tree, GH1* gHist)
 {
     for (Int_t i = 0; i < tree.GetNParticles(); i++)
 	{
         for (Int_t j = 0; j < tagger->GetNTagged(); j++)
 		{
             time = tagger->GetTagged_t(j) - tree.GetTime(i);
-			Htime->Fill(time);
+			gHist->Fill(time);
 		}
 	}
 }
 
-Bool_t PPhysics::IsPrompt(Double_t time, Double_t t_low, Double_t t_high)
+void PPhysics::FillTime(const GTreeParticle& tree, Int_t particle_index, GH1* gHist)
 {
-	if ((time >= t_low) && (time <= t_high)) return kTRUE;
-	
-	return kFALSE;
+	for (Int_t j = 0; j < tagger->GetNTagged(); j++)
+	{
+		time = tagger->GetTagged_t(j) - tree.GetTime(particle_index);
+		gHist->Fill(time);
+	}
 }
-	
-Bool_t PPhysics::IsRandom(Double_t time, Double_t t_low1, Double_t t_high1, 
-										 Double_t t_low2, Double_t t_high2 )
-{
 
-	if ((time >= t_low1) && (time <= t_high1)) return kTRUE;
-	if ((time >= t_low2) && (time <= t_high2)) return kTRUE;
-	
-	return kFALSE;
+void PPhysics::FillMass(const GTreeParticle& tree, GH1* gHist)
+{
+    for (Int_t i = 0; i < tree.GetNParticles(); i++)
+	{
+		gHist->Fill(pi0->Particle(i).M());
+	}
+}
+
+void PPhysics::FillMass(const GTreeParticle& tree, Int_t particle_index, GH1* gHist)
+{
+	gHist->Fill(pi0->Particle(particle_index).M());
 }
 
 Bool_t 	PPhysics::Write()
 {
 	return kTRUE;
 }
-
-void	PPhysics::RandomSubtraction(TH1* prompt, TH1* random, TH1* sub, Double_t ratio)
-{
-	sub->Add(prompt,1);
-	sub->Add(random,-ratio);
-
-}
-
-void	PPhysics::RandomSubtraction(TH3* prompt, TH3* random, TH3* sub, Double_t ratio)
-{
-	sub->Add(prompt,1);
-	sub->Add(random,-ratio);
-
-}
-
-void	PPhysics::ShowTimeCuts(TH1* timeH, TH1* cutsH, Double_t t1, Double_t t2, 
-							 Double_t t3, Double_t t4, Double_t t5, Double_t t6)
-{
-	Double_t t;
-
-	for (Int_t i = timeH->GetXaxis()->FindBin(t1); i < timeH->GetXaxis()->FindBin(t2); i++)
-	{	
-		t = timeH->GetBinContent(i);
-		cutsH->SetBinContent(i,t);
-	}	
-
-	for (Int_t i = timeH->GetXaxis()->FindBin(t3); i < timeH->GetXaxis()->FindBin(t4); i++)
-	{	
-		t = timeH->GetBinContent(i);
-		cutsH->SetBinContent(i,t);
-	}	
-	
-	for (Int_t i = timeH->GetXaxis()->FindBin(t5); i < timeH->GetXaxis()->FindBin(t6); i++)
-	{	
-		t = timeH->GetBinContent(i);
-		cutsH->SetBinContent(i,t);
-	}	
-	
-}
-
-Bool_t	PPhysics::OpenHistFile(const char* pfile, Option_t* option)
-{
-	HistFile = new TFile(pfile, option);
-	if(!HistFile) return kFALSE;
-    if(HistFile->IsZombie()) return kFALSE;
-    
-	cout << "HistFile " << pfile << " opened (option = "<< option << ")." << endl;
-	
-	return kTRUE;
-}
-
-Bool_t 	PPhysics::CloseHistFile()
-{
-	if(!HistFile) return kFALSE;
-	HistFile->Close();
-	
-	return kTRUE;
-}
-
 #endif
